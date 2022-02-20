8af3ad39cf3febf256de757ddd0439aa191f021a
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-02-20 17:00:34 +0100

    update

--- data/script.js
+++ data/script.js
@@ -115,4 +115,64 @@ jokeButton.addEventListener("click", addNewJokeToList);
 //
 //
 //
+// Object Oriented & Factory Functions
+
+// function hex(r, g, b) {
+// 	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// function rgb(r, g, b) {
+// 	return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// function makeColor(r, g, b) {
+// 	const color = {};
+// 	color.r = r;
+// 	color.g = g;
+// 	color.b = b;
+// 	color.rgb = function () {
+// 		const { r, g, b } = this;
+// 		return `rgb(${r}, ${g}, ${b})`;
+// 	};
+// 	color.hex = function () {
+// 		const { r, g, b } = this;
+// 		return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// 	};
+// 	return color;
+// }
+
+// const firstColor = makeColor(70, 120, 210);
+
 //
+//
+//
+//
+//
+
+// Creates a blank, plain JavaScript object;
+// Links (sets the constructor of) this object to another object;
+// Passes the newly created object from Step 1 as the this context;
+// Returns this if the function doesn't return its own object;
+
+function Color(r, g, b) {
+	this.r = r;
+	this.g = g;
+	this.b = b;
+}
+
+Color.prototype.rgb = function () {
+	const { r, g, b } = this;
+	return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.hex = function () {
+	const { r, g, b } = this;
+	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+Color.prototype.rgba = function (a = 1.0) {
+	const { r, g, b } = this;
+	return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
+
+const color1 = new Color(125, 25, 225);
